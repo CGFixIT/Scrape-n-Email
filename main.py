a57f1626d9462385f7838fb0734ac585119cd2f7
Chris Grady | Contact@cgfixit.com <17553614+CGFixIT@users.noreply.github.com>
2026-04-04 12:41:03 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+import mailer  
+
+def main():
+    rcpScraper.scrape()
+    clistScraper.scrape()
+
+if __name__ == "__main__":
+    main()
+    mailer.send_all()  
+
+
+"""
 from bs4 import BeautifulSoup
 import requests, io, os
 import drudgeScraper, rcpScraper, clistScraper
@@ -11,3 +23,4 @@ if __name__ == "__main__":
     main()
     os.system("email.bat")
     quit()
+"""
